485bcf2ff6ea51ebc96ee50eff781855d7b44c71
Iosi Ginerman <iginerman@hotmail.com>
2019-01-16 18:13:51 +0200

    Finished Bayes

--- py_ex2.py
+++ py_ex2.py
@@ -5,6 +5,7 @@ train_file_path = "train.txt"
 test_file_path = "test.txt"
 output_file_path = "output.txt"
 attributes = []
+attributes_to_num_of_types = {}
 train_samples = []
 test_samples = []
 classification_attribute = ""
@@ -18,7 +19,7 @@ def multiply_list(list):
         return result
 
 
-def get_samples_from_file(file_path):
+def get_samples_from_file(file_path, get_types = False):
         samples_as_dicts_list = []
         samples = open(file_path, 'r').read().split('\n')
         for sample in samples[1:]:
@@ -27,7 +28,13 @@ def get_samples_from_file(file_path):
                 for index in range(len(sample_values)):
                         attributes_dict[attributes[index]] = sample_values[index]
                 if (len(attributes_dict) == len(attributes)):        
-                        samples_as_dicts_list.append(attributes_dict)                
+                        samples_as_dicts_list.append(attributes_dict)
+        if (get_types):
+                for att in attributes:
+                        types = set()
+                        for sample in samples_as_dicts_list:
+                                types.add(sample[att])
+                        attributes_to_num_of_types[att] = len(types)       
         return samples_as_dicts_list
 
 
@@ -68,9 +75,7 @@ def decision_tree_algorithm(current_sample):
 def get_conditional_probability(test_value, attribute, current_class):
         num_both_happen = float(sum([1 for sample in train_samples if (sample[attribute] == test_value and sample[classification_attribute] == current_class)]))
         num_class_happens = float(sum([1 for sample in train_samples if sample[classification_attribute] == current_class]))
-        prob_both_happen = num_both_happen / len(train_samples)
-        prob_class_happens = num_class_happens / len(train_samples)
-        conditional_probability = prob_both_happen / prob_class_happens
+        conditional_probability = (num_both_happen + 1) / num_class_happens + attributes_to_num_of_types[attribute]
         return conditional_probability
 
 
@@ -107,16 +112,18 @@ def naive_bayes_algorithm(current_sample):
         most_probable_tuple = all_class_probs[-1] 
         final_classification = most_probable_tuple[0]
 
-        if(prob[1] == (1 / len(possible_classifications)) for prob in all_class_probs): #if all probs are equal
-                final_classification = dumb_rule(possible_classifications)
+        # if(prob[1] == (1 / len(possible_classifications)) for prob in all_class_probs): #if all probs are equal
+        #         final_classification = dumb_rule(possible_classifications)
 
         return final_classification
 
 def get_all_predictions():
         prediction_lists_dict = dict() 
-        dt_predictions = nb_predictions = knn_predictions = []
+        dt_predictions = []
+        nb_predictions = []
+        knn_predictions = []
         lines_list = []
-        lines_list.append("Num\tDT\tKNN\tnaiveBase")
+        lines_list.append("Num\tDT\tKNN\tnaiveBase\t\tACTUAL")
         row_num = 1
         for sample in test_samples:
                 dt = decision_tree_algorithm(sample)
@@ -129,6 +136,8 @@ def get_all_predictions():
                 nb_predictions.append(nb)
                 
                 line = '\t'.join([str(row_num), dt, knn, nb])
+                # REMOVE THIS LINE, JUST FOR TESTING
+                line = line + '\t\t\t\t' + sample[classification_attribute]
                 lines_list.append(line)
                 row_num += 1
         table_as_string = '\n'.join(lines_list)
@@ -139,12 +148,13 @@ def get_all_predictions():
         prediction_lists_dict["KNN"] = knn_predictions
         prediction_lists_dict["NB"] = nb_predictions
         prediction_lists_dict["DT"] = dt_predictions
+
         return prediction_lists_dict
 
 
 if __name__ == "__main__":
         attributes = get_attributes(train_file_path)
-        train_samples = get_samples_from_file(train_file_path)
+        train_samples = get_samples_from_file(train_file_path, True)
         test_samples = get_samples_from_file(test_file_path)
         classification_attribute = attributes[-1]
 
