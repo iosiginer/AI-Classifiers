3543e7e1c3c27957eb3ea6d9dd000c4d38a99e31
Iosi Ginerman <iginerman@hotmail.com>
2019-01-21 22:49:23 +0200

    All but DT implemented

--- py_ex2.py
+++ py_ex2.py
@@ -1,11 +1,13 @@
 from collections import Counter
+from math import log
+from random import randint
 
 # region Global variables
 train_file_path = "train.txt"
 test_file_path = "test.txt"
 output_file_path = "output.txt"
 attributes = []
-attributes_to_num_of_types = {}
+attributes_to_types = {}
 train_samples = []
 test_samples = []
 classification_attribute = ""
@@ -14,17 +16,9 @@ ALG_DEC_TREE = "decision tree"
 ALG_KNN = "knn"
 TAB = '\t'
 NEWLINE = '\n'
-# endregion 
+# endregion
 
-
-# region Helping methods
-
-def multiply_list(list):
-        result = 1
-        for item in list:
-                result = result * item
-        return result
-
+# region Common methods
 
 def get_data_from_file(file_path, get_attributes = False):
         samples_as_dicts_list = []
@@ -43,32 +37,9 @@ def get_data_from_file(file_path, get_attributes = False):
                         types = set()
                         for sample in samples_as_dicts_list:
                                 types.add(sample[att])
-                        attributes_to_num_of_types[att] = len(types)
+                        attributes_to_types[att] = list(types)
                 return samples_as_dicts_list, attributes, attributes[-1]      
         return samples_as_dicts_list
-
-def get_distance_between_samples(current_sample, sample):
-        distance = sum([1 for attribute in attributes[:-1] if current_sample[attribute] != sample[attribute]])
-        return distance
-
-def find_most_frequent_classification(k_closest_neighbours):
-        neighbours_classification = [sample[classification_attribute] for sample in k_closest_neighbours]
-        counter = Counter(neighbours_classification)
-        return counter.most_common(1)[0][0] # return the most common classification from this tuple
-
-def get_conditional_probability(test_value, attribute, current_class):
-        num_both_happen = float(sum([1 for sample in train_samples if (sample[attribute] == test_value and sample[classification_attribute] == current_class)]))
-        num_class_happens = float(sum([1 for sample in train_samples if sample[classification_attribute] == current_class]))
-        conditional_probability = (num_both_happen + 1) / num_class_happens + attributes_to_num_of_types[attribute]
-        return conditional_probability
-
-
-def dumb_rule(possible_classifications):
-        positive_classifications = ["yes", "true", "positive", "skynet"]
-        for option in positive_classifications:
-                if option.lower() in positive_classifications:
-                        return option
-        return positive_classifications[0]
 
 def calculate_accuracies(prediction_lists):
         result = []
@@ -78,9 +49,22 @@ def calculate_accuracies(prediction_lists):
                 good_predictions = sum([1 for index, item in enumerate(list) if item == actual_classifications[index]])
                 result.append(round(float(good_predictions) / num_of_samples, 2))  
         return result
+
+def find_most_frequent_classification(samples_list):
+        samples_classification = [sample[classification_attribute] for sample in samples_list]
+        counter = Counter(samples_classification)
+        return counter.most_common(1)[0][0] # return the most common classification from this tuple
+
 # endregion
 
-# region Algorithm methods
+# region KNN algorithm
+# region Helping Methods
+def get_distance_between_samples(current_sample, sample):
+        distance = sum([1 for attribute in attributes[:-1] if current_sample[attribute] != sample[attribute]])
+        return distance
+
+# endregion Helping Methods
+
 def knn_algorithm(current_sample, k = 5):
         if not (len(current_sample) == len(attributes)):
                 raise AttributeError("The test sample should have the same number of attributes as the training sample")
@@ -94,12 +78,33 @@ def knn_algorithm(current_sample, k = 5):
         prediction = find_most_frequent_classification(k_closest_neighbours)
         return prediction
 
+# endregion
+
+#region Naive Bayes algorithm
+
+#region Helping Methods
+def multiply_list(list):
+        result = 1
+        for item in list:
+                result = result * item
+        return result
+
+def get_conditional_probability(test_value, attribute, current_class):
+        k = len(attributes_to_types[attribute])
+        num_both_happen = float(sum([1 for sample in train_samples if (sample[attribute] == test_value and sample[classification_attribute] == current_class)]))
+        num_class_happens = float(sum([1 for sample in train_samples if sample[classification_attribute] == current_class]))
+        conditional_probability = (num_both_happen + 1) / num_class_happens + k
+        return conditional_probability
 
 
-def decision_tree_algorithm(current_sample):
-        ## TODO second step in finishing
-        return "dt"
+def dumb_rule(possible_classifications):
+        positive_classifications = ["yes", "true", "positive", "skynet"]
+        for option in positive_classifications:
+                if option.lower() in positive_classifications:
+                        return option
+        return positive_classifications[0]
 
+# endregion Helping Methods
 
 def naive_bayes_algorithm(current_sample):
         possible_classifications = list(set([sample[classification_attribute] for sample in train_samples]))
@@ -127,22 +132,53 @@ def naive_bayes_algorithm(current_sample):
         most_probable_tuple = all_class_probs[-1] 
         final_classification = most_probable_tuple[0]
 
-        # if(prob[1] == (1 / len(possible_classifications)) for prob in all_class_probs): #if all probs are equal
-        #         final_classification = dumb_rule(possible_classifications)
+        if (len(set([class_prob_tuple[1] for class_prob_tuple in all_class_probs])) == 1 ): # check if all prob are equal
+                final_classification = dumb_rule(possible_classifications)                
 
         return final_classification
 
 # endregion
 
+# region Decision Tree algorithm
+def create_tree(self, attributes):
+        return DecisionTree(attributes)
+
+# region Helping Methods
+
+
+
+# endregion
+
+class DecisionTree:
+    def __init__(self, attributes, examples):
+
+
+
+    def predict(self, case):
+            """
+            not implemented, returns random value
+            """
+            value = randint(0 , len(attributes_to_types[classification_attribute]) - 1)
+            return attributes_to_types[classification_attribute][value]
+
+
+# endregion Decision Tree algorithm
+
 if __name__ == "__main__":
         train_samples, attributes, classification_attribute = get_data_from_file(train_file_path, get_attributes = True)
         test_samples = get_data_from_file(test_file_path)
+
+        # DIDN'T HAVE TIME TO COMPLETE THE DECISION TREE ALGORITHM
+        # create the decision tree
+        samples = [list(sample.values()) for sample in train_samples]
+        tree = create_tree(attributes, samples)
+        # write_tree_to_file(tree, 'output_tree.txt')
 
         predictions = {ALG_DEC_TREE:[], ALG_KNN:[], ALG_NAIVE_BAYES:[]}
         lines_list = ["Num\tDT\tKNN\tnaiveBase"]
         row_num = 1
         for sample in test_samples:
-                dt = decision_tree_algorithm(sample)
+                dt = tree.predict(sample)
                 knn = knn_algorithm(sample)
                 nb = naive_bayes_algorithm(sample)
 
@@ -151,8 +187,6 @@ if __name__ == "__main__":
                 predictions[ALG_NAIVE_BAYES].append(nb)
                 
                 line = TAB.join([str(row_num), dt, knn, nb])
-                # REMOVE THIS LINE, JUST FOR TESTING
-                line = line + '\t\t\t\t' + sample[classification_attribute]
                 lines_list.append(line)
                 row_num += 1
         # get accuracies
